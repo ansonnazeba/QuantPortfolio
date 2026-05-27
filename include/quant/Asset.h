d4f4bb8736f06ec6e411388a7247b28d14d2a23c
ansonnazeba <ansonnazeba@gmail.com>
2026-05-27 11:02:52 -0400

    Implemented base Asset class

--- include/quant/Asset.h
+++ include/quant/Asset.h
@@ -5,9 +5,26 @@
 #ifndef C_PRACTICE_ASSET_H
 #define C_PRACTICE_ASSET_H
 
+#include <string>
+#include "AssetType.h"
+
 namespace quant {
     class Asset {
+    private:
+        std::string name;
+        AssetType type;
 
+    public:
+        Asset(const std::string& name, AssetType type);
+        virtual ~Asset() = default;
+
+        // getters
+        const std::string& getName() const;
+        AssetType getType() const;
+
+        // virtual methods
+        virtual double getValue() const = 0;
+        virtual std::string getSummary() const = 0;
     };
 }
 
--- src/Asset.cpp
+++ src/Asset.cpp
@@ -3,3 +3,14 @@
 //
 
 #include "../include/quant/Asset.h"
+
+quant::Asset::Asset(const std::string &name, AssetType type):
+    name(name), type(type) {};
+
+const std::string & quant::Asset::getName() const {
+    return name;
+}
+
+quant::AssetType quant::Asset::getType() const {
+    return type;
+}
