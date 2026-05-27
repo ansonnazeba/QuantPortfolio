e9513f4bd989b198b5d4e6fe206beb961e2a5161
ansonnazeba <ansonnazeba@gmail.com>
2026-05-27 18:40:31 -0400

    Implemented Bond asset class

--- include/quant/Bond.h
+++ include/quant/Bond.h
@@ -5,9 +5,32 @@
 #ifndef C_PRACTICE_BOND_H
 #define C_PRACTICE_BOND_H
 
+#include "Asset.h"
 
-class Bond {
-};
+namespace quant {
+    class Bond: public Asset {
+    private:
+        std::string issuer;
+        double faceValue;
+        double couponRate;
+        double marketPrice;
+        int quantity;
+
+    public:
+        Bond(const std::string& name, const std::string& issuer, double faceValue, double couponRate, double marketPrice, int quantity);
+        const std::string& getIssuer() const;
+        double getFaceValue() const;
+        double getCouponRate() const;
+        double getMarketPrice() const;
+        int getQuantity() const;
+        double getAnnualCouponPayment() const;
+
+        void setMarketPrice(double newMP);
+
+        double getValue() const override;
+        std::string getSummary() const override;
+    };
+}
 
 
 #endif //C_PRACTICE_BOND_H
--- src/Bond.cpp
+++ src/Bond.cpp
@@ -3,3 +3,56 @@
 //
 
 #include "../include/quant/Bond.h"
+
+quant::Bond::Bond(const std::string &name, const std::string &issuer, double faceValue, double couponRate,
+    double marketPrice, int quantity):
+        Asset(name, AssetType::Bond), issuer(issuer), faceValue(faceValue), couponRate(couponRate),
+            marketPrice(marketPrice), quantity(quantity) {}
+
+const std::string & quant::Bond::getIssuer() const {
+    return issuer;
+}
+
+double quant::Bond::getFaceValue() const {
+    return faceValue;
+}
+
+double quant::Bond::getCouponRate() const {
+    return couponRate;
+}
+
+double quant::Bond::getMarketPrice() const {
+    return marketPrice;
+}
+
+int quant::Bond::getQuantity() const {
+    return quantity;
+}
+
+double quant::Bond::getAnnualCouponPayment() const {
+    return faceValue * couponRate * quantity;
+
+}
+
+void quant::Bond::setMarketPrice(double newMP) {
+    marketPrice = newMP;
+}
+
+double quant::Bond::getValue() const {
+    return marketPrice * quantity;
+}
+
+std::string quant::Bond::getSummary() const {
+    std::string stOut = "Name: " + getName() + '\n';
+    stOut += "Issuer: " + issuer + '\n';
+    stOut += "Face Value: " + std::to_string(faceValue) + '\n';
+    stOut += "Coupon Rate: " + std::to_string(couponRate) + '\n';
+    stOut += "Market Price: " + std::to_string(marketPrice) + '\n';
+    stOut += "Quantity: " + std::to_string(quantity) + '\n';
+    stOut += "Annual Coupon Payment: " + std::to_string(getAnnualCouponPayment()) + '\n';
+    stOut += "Value: " + std::to_string(getValue()) + '\n';
+
+    return stOut;
+
+
+}
