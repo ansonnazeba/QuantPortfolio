2c3b86a05a8980a00bc37e616dc5458586bf4407
ansonnazeba <ansonnazeba@gmail.com>
2026-05-27 17:49:29 -0400

    Implemented Stock asset class

--- include/quant/Stock.h
+++ include/quant/Stock.h
@@ -5,9 +5,26 @@
 #ifndef C_PRACTICE_STOCK_H
 #define C_PRACTICE_STOCK_H
 
+#include "Asset.h"
 
-class Stock {
-};
+namespace quant {
+    class Stock: public Asset {
+    private:
+        std::string ticker;
+        double shares;
+        double pricePerShare;
+
+    public:
+        Stock(const std::string& name, const std::string& ticker, double shares, double pricePerShare);
+        const std::string& getTicker() const;
+        double getShares() const;
+        double getPricePerShare() const;
+        void setPricePerShare(double pricePerShare);
+
+        double getValue() const override;
+        std::string getSummary() const override;
+    };
+}
 
 
 #endif //C_PRACTICE_STOCK_H
--- src/Stock.cpp
+++ src/Stock.cpp
@@ -3,3 +3,36 @@
 //
 
 #include "../include/quant/Stock.h"
+
+quant::Stock::Stock(const std::string& name, const std::string &ticker, double shares, double pricePerShare):
+Asset(name, AssetType::Stock), ticker(ticker), shares(shares), pricePerShare(pricePerShare) {};
+
+const std::string & quant::Stock::getTicker() const {
+    return ticker;
+}
+
+double quant::Stock::getShares() const {
+    return shares;
+}
+
+double quant::Stock::getPricePerShare() const {
+    return pricePerShare;
+}
+
+void quant::Stock::setPricePerShare(double pricePerShare) {
+    this->pricePerShare = pricePerShare;
+}
+
+double quant::Stock::getValue() const {
+    return pricePerShare * shares;
+}
+
+std::string quant::Stock::getSummary() const {
+    std::string stOut = "Name: " + getName() + '\n';
+    stOut += "Ticker: " + ticker + '\n';
+    stOut += "Shares: " + std::to_string(shares) + '\n';
+    stOut += "Price per share: " + std::to_string(pricePerShare) + '\n';
+    stOut += "Value: " + std::to_string(getValue()) + '\n';
+    return stOut;
+
+}
